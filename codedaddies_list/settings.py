86182640f00fb4d5194baa3f258701f44ecb7710
kazu258078 <andrea258078@gmail.com>
2020-08-04 14:21:20 +0900

    Django-8

--- codedaddies_list/settings.py
+++ codedaddies_list/settings.py
@@ -125,6 +125,6 @@ STATICFILES_DIRS =[
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-# import django_heroku
-# django_heroku.settings(locals())
+import django_heroku
+django_heroku.settings(locals())
 
