5248725f53031aca7f1b1fcc2a8354e609dfcee3
kazu258078 <andrea258078@gmail.com>
2020-08-04 13:51:06 +0900

    Django-8

--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from urllib.parse import quote_plus
+
 from bs4 import BeautifulSoup
 import requests
+
+from .models import Search
+
+BASE_CRAIGSLIST_URL = 'https://losangeles.craigslist.org/search/?query={}'
+BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
 
 # Create your views here.
 def home(request):
@@ -8,8 +15,45 @@ def home(request):
 
 def new_search(request):
     search = request.POST.get('search')
-    print(search)
+    #Search.objects.create(search=search)
+    final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
+    response = requests.get(final_url)
+    data = response.text
+    #print(data)
+    soup = BeautifulSoup(data, features="html.parser")
+    post_list = soup.find_all('li', {'class': 'result-row'})
+    # post_title = post_list[0].find(class_='result-title').text
+    # post_url = post_list[0].find('a').get('href')
+    # post_price = post_list[0].find(class_='result-price').text
+    # print("post_titles: ", type(post_titles[0]))
+    # print("post_titles: ", post_titles[0])
+    # print("post_titles: ", post_titles[0].text)
+    # print("post_titles: ", post_titles[0].get('href'))
+
+    posts = []
+
+    for post in post_list:
+        post_title = post.find(class_='result-title').text
+        post_url = post.find('a').get('href')
+        if post.find(class_='result-price'):
+            post_price = post.find(class_='result-price').text
+        else:
+            post_price = 'N/A'
+
+        if post.find(class_='result-image').get('data-ids'):
+            post_image_id = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+            print(post_image_id)
+            post_image_url = BASE_IMAGE_URL.format(post_image_id)
+            print(post_image_url)
+        else:
+            post_image_url = 'https://craigslist.org/images/peace.jpg'
+
+        posts.append((post_title, post_url, post_price, post_image_url))
+
+
+
     context = {
-        'search': search
+        'search': search,
+        'posts':posts
     }
     return render(request, 'my_app/new_search.html', context)
